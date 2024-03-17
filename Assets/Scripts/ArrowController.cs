b1442f730dbe9b6e996b77845c907f7f105f3503
dogwhy <shafeekhan@gmail.com>
2024-03-17 12:17:49 -0700

    Add Assets folder

---
+++ Assets/Scripts/ArrowController.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowController : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            float rotationAmount = Time.deltaTime * 30f;
+            transform.Rotate(Vector3.down, rotationAmount);
+            Debug.Log("Rotation amount: " + rotationAmount);
+
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(Vector3.up, Time.deltaTime * 30f);
+        }
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            if (transform.localScale.z < 2)
+            {
+                transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y,
+                    transform.localScale.z + (1 * Time.deltaTime));
+            }
+            else
+            {
+                transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y, 2);
+            }
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            if (transform.localScale.z > 0.1f)
+            {
+                transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y,
+                    transform.localScale.z - (1 * Time.deltaTime));
+            }
+            else
+            {
+                transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y, 0.1f);
+            }
+
+        }
+    }
+}
---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,151 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Serialization;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Ball : MonoBehaviour
+{
+
+    public Rigidbody rb; // reference to the Rigidbody component of the ball
+    public float startSpeed = 40f; // the speed at which the ball starts moving
+
+    private Transform _arrow;
+
+    private bool _ballMoving;
+
+    private Transform _startPosition;
+
+    private List<GameObject> _pins = new();
+
+    private readonly Dictionary<GameObject, Transform> _pinsDefaultTransform = new();
+
+    private int previousPinsDown;
+
+    public int Point { get; set; }
+
+
+
+    private TextMeshProUGUI feedBack;
+
+
+    private void Start()
+    {
+        Application.targetFrameRate = 60;
+
+        _arrow = GameObject.FindGameObjectWithTag("Arrow").transform;
+
+        // get the reference to the Rigidbody component of the ball
+        rb = GetComponent<Rigidbody>();
+
+        _startPosition = transform;
+
+        _pins = GameObject.FindGameObjectsWithTag("Pin").ToList();
+
+        foreach (var pin in _pins)
+        {
+            _pinsDefaultTransform.Add(pin, pin.transform);
+        }
+
+        feedBack = GameObject.FindGameObjectWithTag("FeedBack").GetComponent<TextMeshProUGUI>();
+    }
+
+    private void ResetPosition()
+    {
+    transform.position = _startPosition.position; // Reset position to the starting position
+    transform.rotation = _startPosition.rotation; // Reset rotation to the starting rotation
+    rb.velocity = Vector3.zero; // Reset velocity to zero
+    rb.angularVelocity = Vector3.zero; // Reset angular velocity to zero
+    _arrow.gameObject.SetActive(true); // Activate the arrow again
+    _ballMoving = false; // Set ballMoving flag to false
+    }
+
+
+    void Update()
+    {
+        if (_ballMoving)
+        {
+            return;
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            StartCoroutine(Shoot());
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            ResetPosition();
+        }
+
+    }
+
+    private IEnumerator Shoot()
+    {
+
+        _ballMoving = true;
+        _arrow.gameObject.SetActive(false);
+        rb.isKinematic = false;
+
+        // calculate the force vector to apply to the ball
+        Vector3 forceVector = _arrow.forward * (startSpeed * _arrow.transform.localScale.z);
+
+        // calculate the position at which to apply the force (in this case, the center of the ball)
+        Vector3 forcePosition = transform.position + (transform.right * 0.5f);
+
+        // apply the force at the specified position
+        rb.AddForceAtPosition(forceVector, forcePosition, ForceMode.Impulse);
+
+
+        yield return new WaitForSecondsRealtime(7);
+
+        _ballMoving = false;
+
+        GenerateFeedBack();
+
+        yield return new WaitForSecondsRealtime(2);
+
+        ResetGame();
+    }
+
+
+    private static void ResetGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+     private void CalculatePinsDown()
+    {
+        GameObject[] pins = GameObject.FindGameObjectsWithTag("Pin");
+        int pinsDown = 0;
+
+        foreach (var pin in pins)
+        {
+            if (pin.transform.up.y < 0.1f)
+            {
+                pinsDown++;
+            }
+        }
+
+        int fallenPins = pinsDown - previousPinsDown;
+        previousPinsDown = pinsDown;
+
+        // Use fallenPins value as you wish, e.g., updating the score.
+    }
+
+    private void GenerateFeedBack()
+    {
+        feedBack.text = Point switch
+        {
+            0 => "Nothing!",
+            > 0 and < 3 => "That was close!",
+            >= 3 and < 6 => "That was close!",
+            >= 6 and < 10 => "Nice try!",
+            _ => "STRIIIIIIKE"
+        };
+
+    }
+}
